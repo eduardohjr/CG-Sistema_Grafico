4465994342c4e44acb91ac684e405846ab86231e
eduardohjr <104038643+eduardohjr@users.noreply.github.com>
2025-03-19 20:06:02 -0300

    Update graphicObject.py

--- Trabalho1/1.1/src/graphicObject.py
+++ Trabalho1/1.1/src/graphicObject.py
@@ -3,11 +3,11 @@ from abc import ABC, abstractmethod
 class GraphicObjectt(ABC):
     @abstractmethod
     def __init__(self, points, id):
-        self.__id = None
+        self.id = None
         self.__points = []
 
     def getId(self):
-        return self.__id
+        return self.id
     
     def getPoints(self):
         return self.__points
@@ -19,7 +19,7 @@ class GraphicObjectt(ABC):
 class Point(GraphicObjectt):
     def __init__(self, points,id):
         self.__points = points
-        self.__id = id
+        self.id = id
 
     def draw(self, viewport):
         viewport.scene().addEllipse(self.__points[0][0], self.__points[0][1], 5,5, viewport.pen, viewport.blackBurh)
@@ -27,7 +27,7 @@ class Point(GraphicObjectt):
 class Line(GraphicObjectt):
     def __init__(self, points, id):
         self.__points = points
-        self.__id =id
+        self.id =id
 
     def draw(self, viewport):
         viewport.scene().addLine(self.__points[0][0], self.__points[0][1], self.__points[1][0], self.__points[1][1])
@@ -35,11 +35,10 @@ class Line(GraphicObjectt):
 class Polygon(GraphicObjectt):
     def __init__(self, points, id):
         self.__points = points
-        self.__ids =id
+        self.id=id
 
     def draw(self, viewport):
         points = self.__points
         size = len(points)
         for i in range(size):
             viewport.scene().addLine(points[i%size][0], points[i%size][1], points[(i+1)%size][0], points[(i+1)%size][1])
-        
